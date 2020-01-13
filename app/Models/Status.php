5f1e4a012fc9d288bbf28052e72324709cd51fb2
Hui <az2254038@outlook.com>
2020-01-13 09:49:23 +0000

    微博数据模型

---
+++ app/Models/Status.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 微博模型
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+    /**
+     * 一对多模型，一条微博属于一个用户
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
